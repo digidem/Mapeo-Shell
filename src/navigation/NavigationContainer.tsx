df3b3d521d54cbd3908ae8a1e536fcf55f4990fe
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-03-14 15:21:54 -0700

    chore: set up react nav typing

--- src/navigation/NavigationContainer.tsx
+++ src/navigation/NavigationContainer.tsx
@@ -1,9 +1,22 @@
 import { NavigationContainer as NativeNavContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Home } from "../screens/Home";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Bar } from "../screens/sync/Bar";
+import { ProjectCoordinator } from "../screens/projectInvite/ProjectCoordinator";
 
-type Drawers = {
+export type Drawers = {
   Home: undefined;
+  Sync: undefined;
+  ProjectInvite: undefined;
+};
+
+export type SyncScreens = {
+  Bar: undefined;
+};
+
+export type ProjectInviteScreens = {
+  ProjectCoordinator: undefined;
 };
 
 const Drawer = createDrawerNavigator<Drawers>();
@@ -11,9 +24,41 @@ const Drawer = createDrawerNavigator<Drawers>();
 export const NavigationContainer = () => {
   return (
     <NativeNavContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen
+          name="Sync"
+          component={SyncScreensStack}
+          options={{ headerTitle: "" }}
+        />
+        <Drawer.Screen
+          name="ProjectInvite"
+          component={ProjectInviteScreensStack}
+        />
       </Drawer.Navigator>
     </NativeNavContainer>
+  );
+};
+
+const SyncStack = createNativeStackNavigator<SyncScreens>();
+
+const SyncScreensStack = () => {
+  return (
+    <SyncStack.Navigator>
+      <SyncStack.Screen name="Bar" component={Bar} />
+    </SyncStack.Navigator>
+  );
+};
+
+const ProjectInviteStack = createNativeStackNavigator<ProjectInviteScreens>();
+
+const ProjectInviteScreensStack = () => {
+  return (
+    <ProjectInviteStack.Navigator>
+      <ProjectInviteStack.Screen
+        name="ProjectCoordinator"
+        component={ProjectCoordinator}
+      />
+    </ProjectInviteStack.Navigator>
   );
 };
