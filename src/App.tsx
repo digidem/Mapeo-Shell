4abad0937c0923443b507f0701556ddd544b78ee
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-03-18 15:27:27 -0700

    chore: added react-intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import { registerRootComponent } from "expo";
+import { defineMessages } from "react-intl";
+
+const m = defineMessages({
+  firstMessage: {
+    id: "app.firstMessage",
+    defaultMessage: "This is a test",
+    description: "Used as a tester",
+  },
+});
 
 import { NavigationContainer } from "./navigation/NavigationContainer";
 import "react-native-gesture-handler";
