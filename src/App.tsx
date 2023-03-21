1935f9e420f64f865a7e2f5d7d888e7b384f8b20
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-03-21 12:08:43 -0700

    feat: Added Internationalization with Crowdin Integration
    * chore: added react-intl
    
    * chore:added provider
    
    * chore: added formatJS extract messages
    
    * chore: compile translated messages script
    
    * feat: added react-intl and crowdin integration
    
    * chore: removed translations folder
    
    * chore: added build translation script to start script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { registerRootComponent } from "expo";
+import { IntlProvider } from "./contexts/IntlContext";
+import { defineMessages } from "react-intl";
+
+const m = defineMessages({
+  firstMessage: {
+    id: "app.firstMessage",
+    defaultMessage: "This is a test",
+    description: "Used as a tester",
+  },
+});
 
 import { NavigationContainer } from "./navigation/NavigationContainer";
 import "react-native-gesture-handler";
 
 export default function App() {
-  return <NavigationContainer />;
+  return (
+    <IntlProvider>
+      <NavigationContainer />
+    </IntlProvider>
+  );
 }
 
 registerRootComponent(App);
---
+++ src/contexts/IntlContext.tsx
@@ -0,0 +1,95 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useMemo,
+  useState,
+} from "react";
+import { IntlProvider as ReactIntlProvider } from "react-intl";
+import messages from "../../translations/messages.json";
+import languages from "../languages.json";
+
+type TranslatedLocales = keyof typeof messages;
+type SupportedLanguageLocales = keyof typeof languages;
+interface LanguageName {
+  /** IETF BCP 47 langauge tag with region code. */
+  locale: SupportedLanguageLocales;
+  /** Localized name for language */
+  nativeName: string;
+  /** English name for language */
+  englishName: string;
+}
+
+const translatedLocales = Object.keys(messages) as Array<TranslatedLocales>;
+
+export const supportedLanguages: LanguageName[] = translatedLocales
+  .filter((locale) => {
+    const hasAtLeastOneTranslatedString =
+      Object.keys(messages[locale]).length > 0;
+    // This will show a typescript error if the language name does not exist
+    const hasTranslatedLanguageName = languages[locale];
+    if (!hasTranslatedLanguageName) {
+      console.warn(
+        `Locale "${locale}" is not available in Mapeo because we do not have
+a language name and translations in \`src/frontend/languages.json\``
+      );
+    }
+    return hasAtLeastOneTranslatedString && hasTranslatedLanguageName;
+  })
+  .map((locale) => ({
+    locale,
+    ...languages[locale],
+  }))
+  .sort((a, b) => {
+    return a.englishName.localeCompare(b.englishName);
+  });
+
+type IntlSetContextType = Readonly<
+  [string, Dispatch<SetStateAction<TranslatedLocales>>]
+>;
+
+const IntlSetContext = createContext<IntlSetContextType>(["en", () => {}]);
+
+export const IntlProvider = ({ children }: { children: ReactNode }) => {
+  const [appLocale, setAppLocale] = useState<TranslatedLocales>("en");
+
+  const locale = appLocale || getSupportedLocale(appLocale) || "en";
+
+  const languageCode = locale.split("-")[0];
+
+  // Add fallbacks for non-regional locales (e.g. "en" for "en-GB")
+  const localeMessages = {
+    ...messages[languageCode as TranslatedLocales],
+    ...(messages[locale as TranslatedLocales] || {}),
+  };
+
+  const contextValue = useMemo(
+    () => [locale, setAppLocale] as const,
+    [locale, setAppLocale]
+  );
+
+  console.log(translatedLocales);
+
+  return (
+    <ReactIntlProvider
+      locale={locale}
+      messages={localeMessages}
+      wrapRichTextChunksInFragment
+    >
+      <IntlSetContext.Provider value={contextValue}>
+        {children}
+      </IntlSetContext.Provider>
+    </ReactIntlProvider>
+  );
+};
+
+function getSupportedLocale(
+  locale: SupportedLanguageLocales
+): keyof typeof languages | undefined {
+  if (supportedLanguages.find((lang) => lang.locale === locale))
+    return locale as keyof typeof languages;
+  const nonRegionalLocale = locale.split("-")[0];
+  if (supportedLanguages.find(({ locale }) => locale === nonRegionalLocale))
+    return nonRegionalLocale as keyof typeof languages;
+}
