e423f73ed5fa36d5095640e6a12c5d1b89448f8a
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-03 11:37:47 -0700

    chore: updated sync store to include interval

--- src/hooks/stores/useSyncStore.ts
+++ src/hooks/stores/useSyncStore.ts
@@ -18,6 +18,7 @@ type AllSyncs = Record<string, ActiveSync>;
 
 type SyncStore = {
   allSyncs: AllSyncs;
+  intervals: Record<string, NodeJS.Timer>;
   actions: {
     setIndividualSync: (
       deviceId: string,
@@ -25,16 +26,15 @@ type SyncStore = {
       syncGroup: SyncGroup
     ) => void;
     resetSyncStore: () => void;
+    setSyncInterval: (deviceId: string) => void;
+    clearSyncInterval: (deviceId: string) => void;
+    clearAllIntervals: () => void;
   };
 };
 
-const emptyProgress: Progress = {
-  completed: 0,
-  total: 0,
-};
-
-const useSyncStore = create<SyncStore>()((set) => ({
+const useSyncStore = create<SyncStore>()((set, get) => ({
   allSyncs: {},
+  intervals: {},
   actions: {
     setIndividualSync: (deviceId, progress, syncGroup) =>
       set((state) => {
@@ -53,6 +53,54 @@ const useSyncStore = create<SyncStore>()((set) => ({
       set(() => ({
         allSyncs: {},
       })),
+    setSyncInterval: async (deviceId) => {
+      // if sync has already been initiated do nothing
+      if (get().intervals[deviceId] !== undefined) return;
+      const interval: NodeJS.Timer = setInterval(
+        () =>
+          set((state) => {
+            const incrementedCompleted =
+              state.allSyncs[deviceId].progress.completed + 1;
+            if (state.allSyncs[deviceId].syncIsFinished) {
+              state.actions.clearSyncInterval(deviceId);
+            }
+            return {
+              intervals: {
+                ...state.intervals,
+                [deviceId]: interval,
+              },
+              allSyncs: {
+                ...state.allSyncs,
+                [deviceId]: {
+                  progress: {
+                    total: state.allSyncs[deviceId].progress.total,
+                    completed: incrementedCompleted,
+                  },
+                  syncIsFinished:
+                    incrementedCompleted >=
+                    state.allSyncs[deviceId].progress.total,
+                  syncGroup: state.allSyncs[deviceId].syncGroup,
+                },
+              },
+            };
+          }),
+        10
+      );
+    },
+    clearSyncInterval: (deviceId) => {
+      set((state) => {
+        clearInterval(state.intervals[deviceId]);
+        const { [deviceId]: key, ...rest } = state.intervals;
+        return {
+          intervals: rest,
+        };
+      });
+    },
+    clearAllIntervals: () => {
+      Object.keys(get().intervals).map((key) => {
+        get().actions.clearSyncInterval(key);
+      });
+    },
   },
 }));
 
@@ -64,55 +112,58 @@ const useSyncs = () => {
   return useSyncStore((state) => state.allSyncs);
 };
 
-export function useIncompleteSyncs() {
+export function useIncompleteSyncGroup(syncGroup: SyncGroup) {
   const allSyncs = useSyncs();
   return useMemo(
     () =>
       Object.entries(allSyncs)
-        .filter(([, values]) => !values.syncIsFinished)
+        .filter(
+          ([, values]) =>
+            values.syncGroup === syncGroup && !values.syncIsFinished
+        )
         .map(([key]) => allSyncs[key]),
-    [allSyncs]
+    [allSyncs, syncGroup]
   );
 }
 
 /**
  *
  * @param deviceId
- * @returns an array with [sync, setSync(completed:number)]
+ * @returns [progress, startSync]
  */
 export function useIndividualSync(
   deviceId: string,
   syncGroup: SyncGroup
-): readonly [ActiveSync | undefined, () => void] {
+): readonly [Progress | undefined, () => void] {
   const thisSync = useSyncs()[deviceId];
   const setIndividualSync = useSyncAction().setIndividualSync;
+  const setInterval = useSyncAction().setSyncInterval;
 
   if (!thisSync) {
     const total = getRandomNumberMax30();
     setIndividualSync(deviceId, { total, completed: 0 }, syncGroup);
   }
 
-  console.log(thisSync);
-
-  return [
-    thisSync,
+  return useMemo(
     () =>
-      setIndividualSync(
-        deviceId,
-        {
-          total: thisSync.progress.total,
-          completed: thisSync.progress.completed + 1,
+      [
+        !thisSync ? undefined : thisSync.progress,
+        () => {
+          setInterval(deviceId);
         },
-        syncGroup
-      ),
-  ] as const;
+      ] as const,
+    [thisSync, deviceId, setInterval]
+  );
 }
 
 /**
  * @description calling this function will reset sync store. Should be used in cleanup when unmounting the sync screen
  */
-export function useResetSyncStore() {
-  return useSyncAction().resetSyncStore;
+export function useSyncStoreCleanup() {
+  return () => {
+    useSyncAction().resetSyncStore();
+    useSyncAction().clearAllIntervals();
+  };
 }
 
 function getRandomNumberMax30() {
