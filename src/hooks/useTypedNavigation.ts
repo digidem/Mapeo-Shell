3a50502d70fee425aa4119c6dbf87e5454a1147f
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-03 12:16:19 -0700

    chore: remove unused drawer from UI (#16)

--- src/hooks/useTypedNavigation.ts
+++ src/hooks/useTypedNavigation.ts
@@ -2,17 +2,10 @@ import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-import {
-  Drawers,
-  ProjectInviteScreens,
-  SyncScreens,
-} from "../navigation/NavigationContainer";
+import { Drawers, SyncScreens } from "../navigation/NavigationContainer";
 
 export const useNavigationFromDrawer = () =>
   useNavigation<DrawerNavigationProp<Drawers>>();
 
 export const useNavigatioFromSync = () =>
   useNavigation<NativeStackNavigationProp<SyncScreens>>();
-
-export const useNavigationFromProjectInvite = () =>
-  useNavigation<NativeStackNavigationProp<ProjectInviteScreens>>();
