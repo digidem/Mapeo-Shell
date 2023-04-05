b402ff87d29fed56c5d548f205d2f92303fc8d66
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-05 14:54:54 -0700

    chore: ui of progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,31 +1,47 @@
 import * as Progress from "react-native-progress";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { useIndividualSync } from "../hooks/stores/useSyncStore";
-import { useEffect, useRef, useState } from "react";
+import { useSync } from "../hooks/useSyncs";
 
 type ProgressBarProps = {
   deviceId: string;
   deviceName: string;
+  deviceType: "desktop" | "mobile";
+  date: string;
   shouldSync: boolean;
 };
 
-export const ProgressBar = ({ deviceId }: { deviceId: string }) => {
-  const [thisSync, setThisSync] = useIndividualSync(deviceId, "local");
+export const ProgressBar = ({
+  deviceId,
+  deviceName,
+  date,
+  shouldSync,
+  deviceType,
+}: ProgressBarProps) => {
+  const [thisSync, incrementSync] = useSync(deviceId, "local");
+
+  const icon =
+    deviceType === "desktop"
+      ? require("../../assets/desktop.png")
+      : require("../../assets/mobile.png");
 
   return (
-    <TouchableOpacity onPress={setThisSync}>
+    <TouchableOpacity onPress={incrementSync}>
       <Progress.Bar
         color="rgba(0, 102, 255, 0.1)"
-        progress={thisSync ? thisSync.completed / thisSync.total : 0}
+        progress={!thisSync ? 0 : thisSync.completed / thisSync.total}
         width={null}
-        height={80}
+        height={100}
       >
         <View style={styles.container}>
-          <View>
-            <Text>Tester</Text>
+          <View style={styles.nameAndIcon}>
+            <Image
+              style={{ height: 80, width: 80, resizeMode: "contain" }}
+              source={icon}
+            />
+            <Text style={{ margin: 10 }}>{deviceName}</Text>
           </View>
-          <Text>Tester</Text>
+          <Text style={{ fontSize: 12 }}>{date}</Text>
         </View>
       </Progress.Bar>
     </TouchableOpacity>
@@ -42,5 +58,10 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     padding: 20,
+  },
+  nameAndIcon: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
