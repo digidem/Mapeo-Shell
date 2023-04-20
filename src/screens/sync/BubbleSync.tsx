ba8295cfe763669e5dc38a21acd2e5322b753bfe
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-20 11:38:08 -0700

    chore: scaffold

--- src/screens/sync/BubbleSync.tsx
+++ src/screens/sync/BubbleSync.tsx
@@ -1,1 +1,15 @@
-export const BubbleSync = () => {};
+import { useContext } from "react";
+import { ScrollView } from "react-native";
+import { SyncContext } from "../../contexts/SyncContext";
+
+export const BubbleSync = () => {
+  const [syncs] = useContext(SyncContext);
+
+  const activeSyncs = Object.entries(syncs.local).some(
+    ([keys, val]) => val === "active"
+  );
+
+  const bubbleImage = require("../../../assets/bubblesSyncing.png");
+
+  return <ScrollView></ScrollView>;
+};
